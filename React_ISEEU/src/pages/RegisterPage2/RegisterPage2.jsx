0ba6d94467773e882c04e23dca5be6b93798582d
TalalEmara <talalmemara@gmail.com>
2024-05-17 22:13:31 +0300

    improved the Picture and the inputs--Inputs is scrollable now
    Gender needs label  and the specifics of the photo

--- React_ISEEU/src/pages/RegisterPage2/RegisterPage2.jsx
+++ React_ISEEU/src/pages/RegisterPage2/RegisterPage2.jsx
@@ -11,21 +11,27 @@ const Register2 = () => {
                         <form>
                             <div className={"row"}>
                                 <div id={"profilePreview"} className={"col-3"}>
-                                    <img src={""} alt={"Preview"}/>
+                                    <img src={"https://placehold.co/320x320"} alt={"Preview"}/>
                                     <input type = "file"/>
                                     <p>snsna<br/>sfasfsaa<br/>ssads</p>
+                                    <Btn label = "Back" type={"submit"}/>
+
                                 </div>
                                 <div id={"inputs"} className={"col-9"}>
                                     <p>Personal Info</p>
-                                    <UserText1/>
-                                    <OR></OR>
-                                    <UserText1/>
-                                    <UserText1/>
-                                    <UserText1/>
-                                    <UserText1/>
-                                    <UserText1/>
+                                    <div className={"row"}>
+                                        <div className={"col-6"}><UserText1 label="First name" type={"text"}/></div>
+                                        <div className={"col-6"}><UserText1 label="First name" type={"text"}/></div>
+                                    </div>
+                                    <UserText1 label="Date of birth" type={"date"}/>
+                                    <OR/>
+                                    <UserText1 label="Address" type={"text"}/>
+                                    <UserText1 label="Email" type={"email"}/>
+                                    <UserText1 label="Phone" type={"number"}/>
 
+                                <Btn label = "Next" type={"submit"}/>
                                 </div>
+
                             </div>
                         </form>
                     </div>
