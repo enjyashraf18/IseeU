73e60c53445ae980411259ae6e01287924207311
TalalEmara <talalmemara@gmail.com>
2024-05-19 20:14:57 +0300

    Register 2 is done _need some modificatatiosn_
    Gender label is add to OR BTN
    Register2 need to be changed:
    Specs of photos
    Onchange handling(Fake Path)
    **Problem of the same name across the pages -CSS-***

--- React_ISEEU/src/pages/RegisterPage2/RegisterPage2.jsx
+++ React_ISEEU/src/pages/RegisterPage2/RegisterPage2.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./RegisterPage2.css";
 import { OR, MBut, DEL, Search, UserText1, UserText2, UserAge, CheckBox, OpenLi, EmerBtn, Btn, LiBTN } from '../../components';
 
 const Register2 = () => {
+
+    const [profileImg,  setProfileImg] = useState("https://placehold.co/320x320")
+    function  handleImgupload(e){
+        setProfileImg(e.target.value);
+        console.log(profileImg);
+    }
+
+
     return (<div id="container">
             <div className="container-fluid">
                 <div className="row">
@@ -11,14 +19,12 @@ const Register2 = () => {
                         <form>
                             <div className={"row"}>
                                 <div id={"profilePreview"} className={"col-3"}>
-                                    <img src={"https://placehold.co/320x320"} alt={"Preview"}/>
-                                    <input type = "file"/>
-                                    <p>snsna<br/>sfasfsaa<br/>ssads</p>
-                                    <Btn label = "Back" type={"submit"}/>
-
+                                    <img src={profileImg} alt={"Preview"}/>
+                                    <input onChange={handleImgupload} type = "file"/>
+                                    <p id={"imgSpecs"}>snsna<br/>sfasfsaa<br/>ssads</p>
                                 </div>
                                 <div id={"inputs"} className={"col-9"}>
-                                    <p>Personal Info</p>
+                                    <p id={"scndTitle"}>Personal Info</p>
                                     <div className={"row"}>
                                         <div className={"col-6"}><UserText1 label="First name" type={"text"}/></div>
                                         <div className={"col-6"}><UserText1 label="First name" type={"text"}/></div>
@@ -29,9 +35,12 @@ const Register2 = () => {
                                     <UserText1 label="Email" type={"email"}/>
                                     <UserText1 label="Phone" type={"number"}/>
 
-                                <Btn label = "Next" type={"submit"}/>
-                                </div>
 
+                            <div className={"row"}>
+                                <div className={"col-1"}><Btn label="Back" type={"submit"}/></div>
+                                <div className={"col-1 offset-8"}> <Btn id = "nxtBtn" label = "Next" type={"submit"}/> </div>
+                            </div>
+                                </div>
                             </div>
                         </form>
                     </div>
