73e57020932dc7de1bf8bda15a972560ab698919
Ahmed Al-Deeb <124098788+AhmedXAlDeeb@users.noreply.github.com>
2024-05-20 15:03:44 +0300

    Login functionalities - Aldeeb

--- React_ISEEU/src/pages/LoginPage/Login.jsx
+++ React_ISEEU/src/pages/LoginPage/Login.jsx
@@ -3,6 +3,8 @@ import './Login.css'; // Assuming you have some custom CSS here
 import { useForm } from 'react-hook-form';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { OR,MBut,DEL,Search,UserText1,UserText2,UserAge,CheckBox,OpenLi,EmerBtn,Btn,LiBTN } from '../../components';
+
 
 function Login() {
   const { register, handleSubmit,reset ,formState: { errors } } = useForm();
@@ -12,20 +14,86 @@ function Login() {
   };
 
 
-  
+const submitForm = (data) => {
+      const body = {
+          email: data.email,
+          password: data.password
+      }
+
+      const requestOptions = {
+          method: "POST",
+          headers: {
+              'content-type': 'application/json'
+          },
+          body: JSON.stringify(body)
+      }
+
+      fetch('/auth/login', requestOptions)
+      .then(res => res.json())
+      .then(data =>{
+          console.log(data)
+          // setServerResponse(data.message)
+          // setShow(true)
+      })
+      .catch(err => console.log(err))
+
+  reset()
+    }
+
+
+
+
   return (
-    <div id='fullPage'>
+    <Container fluid id='fullPage'>
       <Row>
-        <Col id="rightCol" md={8} >
-          <h2>Welcome Back!</h2>
-          <p>Please login to your account.</p>
+        <div id="left">
+        <Col  xl={6}>
+        <div 
+        style=
+        {{width: '307px',
+        height: '115px',
+
+        fontFamily: "'Radio Canada'",
+        fontStyle: 'normal',
+        fontWeight: 400,
+        fontSize: '95px',
+        lineHeight: '120.69%',
+        color: '#e0e0e0'}}
+        >ISEE<br></br></div>
+        <div
+          style=
+          {{
+          width: '307px',
+          height: '115px',
+          marginleft: "5px",
+          fontFamily: "'Radio Canada'",
+          fontStyle: 'normal',
+          fontWeight: 2000,
+          fontSize: '380px',
+          lineHeight: '70%',
+          color: '#e0e0e0'}}
+        >U
+        </div>
         </Col>
-        <Col id="container" md={4}>
-          <h2>Login</h2>
-          <Form onSubmit={handleSubmit(onSubmit)}>
-            <Form.Group controlId="formBasicEmail">
+        </div>
+        <Col id="right"xl={6}>
+        <div>
+        <h1 style={{
+          width: '307px',
+          height: '115px',
+
+          fontFamily: "'Radio Canada'",
+          fontStyle: 'normal',
+          fontWeight: 700,
+          fontSize: '95px',
+          lineHeight: '120.69%',
+          color: '#258176',
+          }}>Login</h1>
+          </div>
+          <Form id= "loginForm"onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group controlId="formBasicEmail" className="form-group">
               <Form.Label>Email address</Form.Label>
-              <Form.Control 
+              <Form.Control
                 type="email" 
                 placeholder="Enter email"
                 {...register("email", { required: true, maxLength: 80 })}
@@ -33,7 +101,7 @@ function Login() {
               {errors.email && <span >Email is required</span>}
             </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
+            <Form.Group controlId="formBasicPassword" className="form-group">
               <Form.Label>Password</Form.Label>
               <Form.Control 
                 type="password" 
@@ -45,19 +113,16 @@ function Login() {
 
             </Form.Group>
 
-            <Button 
-            as= "sub" 
-            variant="primary" 
-            type="submit"
-            onClick={handleSubmit(onSubmit)} >
+            <Button variant="primary" type="submit">
               Submit
             </Button>
           </Form>
           <Link to='/register'>New Admin? Register</Link>
         </Col>
-      </Row>
-    </div>
+        </Row>
+    </Container>
   );
 }
 
 export default Login;
+
