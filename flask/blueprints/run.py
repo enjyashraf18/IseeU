bcf8eb4c309a0c13a33df248a9f83688eb73be1f
H252003 <mohamedhesham252003@gmail.com>
2024-06-14 00:32:20 +0300

    nearly all pages functions place holders

--- flask/blueprints/run.py
+++ flask/blueprints/run.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from login import login
 from register import register
+from nurse_view import nurse_view
+from doctor_view import doctor_view
+from admin_view import admin_view
 
 app = Flask(__name__)
 app.register_blueprint(login)  #, url_prefix="/login"
 app.register_blueprint(register)
+app.register_blueprint(nurse_view)
+app.register_blueprint(doctor_view)
+app.register_blueprint(admin_view)
+
+
 
 app.config["SECRET_KEY"] = "SECRET_KEY"
 
