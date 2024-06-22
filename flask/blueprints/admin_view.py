1261405045550dbc17e4f86674a2b8086a56f13c
Enjy <enjyashraf18@gmail.com>
2024-06-22 16:06:26 +0300

    replacing post with get

--- flask/blueprints/admin_view.py
+++ flask/blueprints/admin_view.py
@@ -230,9 +230,9 @@ def available_beds(bed_type):
     else:
         return jsonify({"error": "No available beds of this type"}), 400
 
-@admin_view.route('/admin/doctors', methods=['POST'])
+@admin_view.route('/admin/doctors', methods=['GET'])
 def all_doctors():
-    
+
     # return all the doctors
     cursor.execute("""
            SELECT *
@@ -244,7 +244,7 @@ def all_doctors():
     return jsonify({"all_doctors": doctors})
 
 
-@admin_view.route('/admin/nurses', methods=['POST'])
+@admin_view.route('/admin/nurses', methods=['GET'])
 def all_nurses():
 
     # return all the nurses
